bf4f4572891d04926a9014a46993a6f309b35d0b
Franco Polvora <francopolvora@gmail.com>
2026-07-23 19:38:19 -0300

    Changes and additions: Creation of folders for images, external/custom font, styles, and page components. Implementation of the page logo and theme change button (non-functional).

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import logo from "../assets/img/logo.png"
+import cambioDeTema from "../assets/img/cambio-de-tema.png"
+import '../styles/header.css'
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img className="imagenLogo" src={logo} alt="Logo" />
+            <h2>Character Counter</h2>
+            <button><img className="imagenBoton" src={cambioDeTema} alt="boton de cambio de tema" /></button>
+        </header>
+    )
+}
+
+export {Header}

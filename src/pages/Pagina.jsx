33f4427b92116bfdfd0cc74b6e976fe5a51f87ae
Franco Polvora <francopolvora@gmail.com>
2026-07-24 00:02:06 -0300

    Additions: Textarea, checkboxes, and approximate reading

--- src/pages/Pagina.jsx
+++ src/pages/Pagina.jsx
@@ -7,6 +7,24 @@ function Pagina() {
   return (
     <>
       <Header />
+      <h1>Analyze your text in real-time</h1>
+      <div className="contenedor-textarea">
+        <textarea
+        placeholder="Escribe tu texto..."
+      ></textarea>
+      </div>
+      <div className="contenedor-checkbox">
+        <label>
+          <input type="checkbox" /> 
+          <span>Excluir espacios</span>
+        </label>
+        <label>
+          <input type="checkbox" />
+          <span>Limite de caracteres</span>
+        </label>
+        <p>Lectura aproximada: </p>
+      </div>
+
     </>
   )
 }
